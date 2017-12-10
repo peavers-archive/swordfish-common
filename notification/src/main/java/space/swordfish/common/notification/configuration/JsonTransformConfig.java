f7732192c97c9850be385af4c9cd5ad1b464864d
Chris Turner <chris.turner@education.govt.nz>
2017-12-11 12:48:22 +1300

    * refactor and rename of libraries

--- notification/src/main/java/space/swordfish/common/notification/configuration/JsonTransformConfig.java
+++ notification/src/main/java/space/swordfish/common/notification/configuration/JsonTransformConfig.java
@@ -6,9 +6,6 @@ import com.github.jasminb.jsonapi.ResourceConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import space.swordfish.common.notification.domain.Notification;
-import space.swordfish.common.services.JsonTransformService;
-import space.swordfish.common.services.JsonTransformServiceImpl;
-
 
 @Configuration
 public class JsonTransformConfig {
