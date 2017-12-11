36a05581b98311f621257774448737af85d4581c
Chris Turner <peavers@gmail.com>
2017-12-11 18:28:49 +1300

    * added default bean

--- notification/src/main/java/space/swordfish/common/notification/services/NotificationServiceImpl.java
+++ notification/src/main/java/space/swordfish/common/notification/services/NotificationServiceImpl.java
@@ -1,6 +1,5 @@
 package space.swordfish.common.notification.services;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
