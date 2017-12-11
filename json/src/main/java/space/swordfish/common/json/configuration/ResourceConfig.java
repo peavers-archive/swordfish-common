b53964d7a905b15cbee8f4c86c5851e13dccbfaa
Chris Turner <peavers@gmail.com>
2017-12-11 19:07:03 +1300

    * added default bean

--- json/src/main/java/space/swordfish/common/json/configuration/ResourceConfig.java
+++ json/src/main/java/space/swordfish/common/json/configuration/ResourceConfig.java
@@ -1,11 +1,13 @@
 package space.swordfish.common.json.configuration;
 
 import com.github.jasminb.jsonapi.ResourceConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ResourceConfig {
 
+    @Bean
     public ResourceConverter resourceConverter() {
         return new ResourceConverter();
     }
