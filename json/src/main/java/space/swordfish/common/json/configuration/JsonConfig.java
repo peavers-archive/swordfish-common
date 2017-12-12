139ad22dae91951a4d0e030d86923f69aaf8780d
Chris Turner <peavers@gmail.com>
2017-12-13 09:42:44 +1300

    * added qualifier for queue config

--- json/src/main/java/space/swordfish/common/json/configuration/JsonConfig.java
+++ json/src/main/java/space/swordfish/common/json/configuration/JsonConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ResourceConfig {
+public class JsonConfig {
 
     @Bean
     public ResourceConverter resourceConverter() {
--- queue/src/main/java/space/swordfish/common/queue/configuration/QueueConfig.java
+++ queue/src/main/java/space/swordfish/common/queue/configuration/QueueConfig.java
@@ -1,25 +1,17 @@
-package space.swordfish.common.notification.configuration;
+package space.swordfish.common.queue.configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.github.jasminb.jsonapi.ResourceConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import space.swordfish.common.json.services.JsonTransformService;
-import space.swordfish.common.json.services.JsonTransformServiceImpl;
-import space.swordfish.common.notification.domain.Notification;
-import space.swordfish.common.notification.services.NotificationService;
-import space.swordfish.common.notification.services.NotificationServiceImpl;
 
 @Configuration
-public class NotificationConfig {
+public class QueueConfig {
 
     @Value("${aws.accessKey}")
     private String accessKey;
@@ -29,22 +21,6 @@ public class NotificationConfig {
 
     @Value("${aws.region}")
     private String region;
-
-    @Bean
-    public NotificationService notificationService() {
-        return new NotificationServiceImpl();
-    }
-
-    @Bean
-    public JsonTransformService jsonTransformService() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategy.KEBAB_CASE);
-
-        ResourceConverter resourceConverter = new ResourceConverter(objectMapper,
-                Notification.class);
-
-        return new JsonTransformServiceImpl(resourceConverter);
-    }
 
     @Bean
     @Primary
