ab178bde6287d3f4d79e6d443341ca1f0a8a2ab2
Chris Turner <peavers@gmail.com>
2017-12-22 15:11:41 +1300

    * give each new notification a random id. This makes sense as they are only used to transport information about and never saved to a db;

--- notification/src/main/java/space/swordfish/common/notification/domain/Notification.java
+++ notification/src/main/java/space/swordfish/common/notification/domain/Notification.java
@@ -4,14 +4,16 @@ import com.github.jasminb.jsonapi.annotations.Id;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Type("notifications")
 public class Notification {
 
-	@Id
-	String id;
-	String channel;
-	String event;
-	String message;
+    @Id
+    String id = UUID.randomUUID().toString();
+    String channel;
+    String event;
+    String message;
 
 }
